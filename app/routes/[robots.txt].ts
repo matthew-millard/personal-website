8c05d6f7fb525ea0f3b135ce9bb4ca1399464e6c
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-01-27 21:59:14 -0500

    feat: add the functionality and route for a robots.txt file

---
+++ app/routes/[robots.txt].ts
@@ -0,0 +1,23 @@
+import { ENV } from "env";
+
+export async function loader() {
+  const baseUrl = ENV.BASE_URL;
+
+  if (!baseUrl) {
+    console.error("BASE_URL is not defined");
+    return new Response("Internal server error", { status: 500 });
+  }
+
+  const robotText = `
+        User-agent: Googlebot
+        Disallow: /nogooglebot/
+        User-agent: *
+        Allow: /
+        Sitemap: https://${baseUrl}/sitemap.xml`;
+  return new Response(robotText, {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    status: 200,
+  });
+}
